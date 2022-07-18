d7d9a4ba238e43a85071c01e3e17296c4f66472d
Felipe <felipedossantosflor@gmail.com>
2022-07-18 18:18:42 -0300

    Exercicio

--- Calculadora/Calculadora/Program.cs
+++ Calculadora/Calculadora/Program.cs
@@ -20,57 +20,9 @@ namespace Calculadora
                 n2 = (Convert.ToInt32(Console.ReadLine()));
 
                 Console.WriteLine("Wich operation you want to do? 1-Sum, 2-Minus, 3-Multiply ou 4-Division");
-                int operacao = Convert.ToInt32(Console.ReadLine());
+                int operation = Convert.ToInt32(Console.ReadLine());
 
-                switch (operacao)
-                {
-                    case 1:
-                        Sum(n1, n2);
-                        break;
-
-                    case 2:
-                        Minus(n1, n2);
-                        break;
-
-                    case 3:
-                        Multiply(n1, n2);
-                        break;
-
-                    case 4:
-                        Division(n1, n2);
-                        break;
-                }
-
-                
-                static int Sum(int a, int b)
-                {
-                    int result = a + b;
-                    return result;
-                    Console.WriteLine(a + " + " + b + " = " + result);
-                }
-
-                static int Minus(int a, int b)
-                {
-                    int result = a - b;
-                    return result;
-                    Console.WriteLine(a + "-" + b + " = " + result);
-                }
-
-                static int Multiply(int a, int b)
-                {
-                    int result = a * b;
-                    return result;
-                    Console.WriteLine(a + "x" + b + " = " + result);
-                }
-
-                static int Division(int a, int b)
-                {
-                    int result = a / b;
-                    return result;
-                    Console.WriteLine(a + "/" + b + " = " + result);
-                }
-
-                
+                Print(operation, n1, n2);
 
                 Console.WriteLine("Do you want to continue? 1 to yes ou 2 to no");
                 x = (Convert.ToInt32(Console.ReadLine()));
@@ -78,5 +30,52 @@ namespace Calculadora
             } while ( x == 1 );
 
         }
+
+        static int Sum(int a, int b)
+        {
+            int result = a + b;
+            return result;
+        }
+
+        static int Minus(int a, int b)
+        {
+            int result = a - b;
+            return result;
+        }
+
+        static int Multiply(int a, int b)
+        {
+            int result = a * b;
+            return result;
+        }
+
+        static int Division(int a, int b)
+        {
+            int result = a / b;
+            return result;
+        }
+
+        static void Print(int operation, int n1, int n2)
+        {
+            switch (operation)
+            {
+                case 1:
+                    Console.WriteLine(n1 + " + " + n2 + " = " + Sum(n1, n2));
+                    break;
+
+                case 2:
+                    Console.WriteLine(n1 + "-" + n2 + " = " + Minus(n1, n2));
+                    break;
+
+                case 3:
+                    Console.WriteLine(n1 + "x" + n2 + " = " + Multiply(n1, n2));
+                    break;
+
+                case 4:
+                    Console.WriteLine(n1 + "/" + n2 + " = " + Division(n1, n2));
+                    break;
+            }
+        }
+
     }
 }
